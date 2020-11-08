958c126a6b723220ed3b09fdaf37dbf95036603f
Nazar Kalapun <ntkalapun@gmail.com>
2020-11-09 01:22:11 +0200

    we should enter number of variant in the endpoint

--- core.py
+++ core.py
@@ -1,14 +1,16 @@
 import flask
 
-
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
 
 
 @app.route("/api/v1/hello-world-<int:variant>", methods=["GET"])
-def home(variant):
+def number(variant):
     """tut bude text"""
     return f"Hello world {variant}"
 
 
-app.run()
+
+
+if __name__ == '__main__':
+    app.run()
