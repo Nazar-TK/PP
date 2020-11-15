6b9462f5edc837e0a923b23eb1cb3eb2fa330a8b
Nazar Kalapun <ntkalapun@gmail.com>
2020-11-16 01:36:33 +0200

    Combined all together

--- core.py
+++ core.py
@@ -1,16 +1,15 @@
-
 import flask
 from wsgiref.simple_server import make_server
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
 
+
 @app.route("/api/v1/hello-world-<int:variant>", methods=["GET"])
-def index(variant):
-    """Endpoint return number of variant(must be int)"""
-    return f"Hello world"
+def number(variant):
+    """tut bude text"""
+    return f"Hello world {variant}"
+if __name__ == '__main__':
+    with make_server('', 5000, app) as server:
+        print("pahaje!")
 
-
-with make_server('', 5000, app) as server:
-    print("F")
-
-server.serve_forever()
+        server.serve_forever()
